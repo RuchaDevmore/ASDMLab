274a1fd93967ec036c920970fe89153296c23c95
RuchaDevmore <128315267+RuchaDevmore@users.noreply.github.com>
2023-05-05 10:00:17 +0530

    Update Q7Pattern1.java
    changes_on_remote_server

--- Q7Pattern1.java
+++ Q7Pattern1.java
@@ -6,6 +6,7 @@
 public class Q7Pattern1 {
 
 	public static void main(String[] args) {
+System.out.println(<< "Perform changes on git Remote repo >>");
 		for(int i=1;i<=4;i++)
 		{
 			for(int j=1;j<=10;j++)
